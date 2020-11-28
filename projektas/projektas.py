a7b144ca7fe922d22af9558670ae222c850f8497
Lukas Žaromskis <lzaromskis@gmail.com>
2020-11-28 18:51:07 +0200

    updated project

--- projektas/projektas.py
+++ projektas/projektas.py
@@ -49,14 +49,69 @@ def turnRight(client, leftMotor, rightMotor, speed):
     retRight = sim.simxSetJointTargetVelocity(client, rightMotor, 0, sim.simx_opmode_streaming)
     return retLeft | retRight
 
+def stop(client, leftMotor, rightMotor):
+    retLeft = sim.simxSetJointTargetVelocity(client, leftMotor, 0, sim.simx_opmode_oneshot_wait)
+    retRight = sim.simxSetJointTargetVelocity(client, rightMotor, 0, sim.simx_opmode_oneshot_wait)
+    return retLeft | retRight
+
+def getPosition(client, handle):
+    retVal, pos = sim.simxGetObjectPosition(client, handle, -1, sim.simx_opmode_oneshot_wait)
+    return pos
+
+def getRotation(client, handle):
+    retVal, rot = sim.simxGetObjectOrientation(client, handle, -1, sim.simx_opmode_oneshot_wait)
+    return rot
+
+def isApproximatePosition(source, dest, error):
+    retVal = True;
+    retVal = retVal & (abs(source[0] - dest[0]) < error)
+    retVal = retVal & (abs(source[1] - dest[1]) < error)
+    retVal = retVal & (abs(source[2] - dest[2]) < error)
+    return retVal
+
+def isApproximateRotation(source, dest, error):
+    return abs(source - dest) < error
+
+def rotateUntil(client, robot, leftMotor, rightMotor, angle):
+    rot = getRotation(client, robot)
+    print('Current rotation: ' + str(rot[2]) + '\nDesired rotation: ' + str(angle))
+    if angle < rot[2]:
+        while not isApproximateRotation(rot[2], angle, 0.01):
+            turnLeft(client, leftMotor, rightMotor, 0.2)
+            rot = getRotation(client, robot)
+    elif angle > rot[2]:
+        while not isApproximateRotation(rot[2], angle, 0.01):
+            turnRight(client, leftMotor, rightMotor, 0.2)
+            rot = getRotation(client, robot)
+    stop(client, leftMotor, rightMotor)
+
+def getDesiredRotation(source, target):
+    return np.arctan2(target[1] - source[1], target[0] - source[1])
+
+def bug0(client, robot, leftMotor, rightMotor, sensors):
+    destHandle = getHandle(client, 'destination1')
+    destPos = getPosition(client, destHandle)
+    robPos = getPosition(client, robot)
+    
+    desAngle = getDesiredRotation(robPos, destPos)
+    rotateUntil(client, robot, leftMotor, rightMotor, desAngle)
+
+    while not isApproximatePosition(robPos, destPos, 0.2):
+        moveForward(client, leftMotor, rightMotor, 1)
+        robPos = getPosition(client, robot)
+    stop(client, leftMotor, rightMotor)
+
 def main():
     clientID = connect()
+    robot = getHandle(clientID, 'Pioneer_p3dx')
     leftMotor = getHandle(clientID, 'Pioneer_p3dx_leftMotor')
     rightMotor = getHandle(clientID, 'Pioneer_p3dx_rightMotor')
     sensors = []
     for i in range(16):
         sensor = getHandle(clientID, 'Pioneer_p3dx_ultrasonicSensor' + str(i + 1))
-
+    
+    bug0(clientID, robot, leftMotor, rightMotor, sensors)
+    stop(clientID, leftMotor, rightMotor)
     disconnect(clientID)
 
 if __name__ == "__main__":
